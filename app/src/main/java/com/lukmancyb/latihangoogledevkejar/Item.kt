e075ad8eb2ab9a11c6bdb5d7767413361839d6c3
lukmancyb <lukman821@gmail.com>
2018-10-15 19:34:55 -0700

    Initial commit

---
+++ app/src/main/java/com/lukmancyb/latihangoogledevkejar/Item.kt
@@ -0,0 +1,10 @@
+package com.lukmancyb.latihangoogledevkejar
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Created by TOSHIBA on 27/08/2018.
+ */
+
+data class Item (val name : String?, val image: Int?, val deskripsi: String?)
